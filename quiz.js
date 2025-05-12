e450c960c2ba97d57020a332bd664c5db2da79dd
Vincenzo Davi <vinny@daviworld.net>
2025-05-12 12:36:31 -0400

    Added quiz.html

--- quiz.js
+++ quiz.js
@@ -5,3 +5,11 @@ function checkAnswer(answer) {
         document.getElementById("result").innerText = "Try again!";
     }
 }
+
+/*
+git init
+git remote add origin {remote_repository_url}
+git add .
+git commit -m "Initial commit"
+git push -u origin main
+*/
